e7105ebfcfaacc5421d59bd47cbbc9604010ade4
notzer0 <24914325+i386sh@users.noreply.github.com>
2022-11-03 21:55:05 +1100

    uuuuuuuuuuuuuuuuuuuuuuuuuuhhhhhhhhhhhh

--- common.js
+++ common.js
@@ -1,3 +1,4 @@
+// I also really want to mention that this sucks ass, and I know it does. Didn't really want to use jQuery. Sorry.
 function toggle(btn, element, avoidScroll) {
     let _x = element.style.display;
     if(_x == "none") {
