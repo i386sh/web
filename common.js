cd4045341ac3bda28a642e7aaae0ed7bb98b39bf
notzer0 <24914325+i386sh@users.noreply.github.com>
2022-11-19 22:38:58 +1100

    I do a thing
    yea

--- common.js
+++ common.js
@@ -1,4 +1,10 @@
 // this is the laziest way i could have swapped to jquery for toggling elements. do not care it works lol
 function toggle(btn,element,avoidScroll) {
-    $(element).toggle(400,"swing")
+    $(element).toggle({
+        duration:150,
+        easing:"swing",
+        done: function() {
+            if(!avoidScroll || $(element).css("display") !== "none") btn.scrollIntoView()
+        }
+    })
 }
