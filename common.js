9e6453a1d8070f8d7ea626b5bd15c91ff5ed7686
notzer0 <24914325+i386sh@users.noreply.github.com>
2022-11-19 13:57:47 +1100

    jquery because I realized doing this myself sucked
    lol

--- common.js
+++ common.js
@@ -1,11 +1,4 @@
-// I also really want to mention that this sucks ass, and I know it does. Didn't really want to use jQuery. Sorry.
-function toggle(btn, element, avoidScroll) {
-    let _x = element.style.display;
-    if(_x == "none") {
-        element.style.removeProperty("display");
-        if(!avoidScroll)setTimeout(()=>{btn.scrollIntoView({behavior: "auto", block: "start"});},1)
-    } else {
-        element.style.setProperty("display","none");
-        if(!avoidScroll)setTimeout(()=>{btn.scrollIntoView({behavior: "auto", block: "start"});},1)
-    }
+// this is the laziest way i could have swapped to jquery for toggling elements. do not care it works lol
+function toggle(btn,element,avoidScroll) {
+    $(element).toggle(400,"swing")
 }
